69c4d76e759ba0def30654a35f98065266844bc6
raydanielg <airezra2+1@gmail.com>
2026-04-12 13:16:46 +0300

    commit

--- app/Models/NewsletterSubscription.php
+++ app/Models/NewsletterSubscription.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class NewsletterSubscription extends Model
 {
+    use Notifiable;
+
     protected $fillable = ['email', 'is_active'];
+
+    /**
+     * Route notifications for the mail channel.
+     */
+    public function routeNotificationForMail(): string
+    {
+        return $this->email;
+    }
 }
