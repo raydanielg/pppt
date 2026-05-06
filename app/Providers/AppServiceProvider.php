daa32ae9ecf9583b915001d291d56726003e2652
raydanielg <airezra2+1@gmail.com>
2026-05-06 22:13:39 +0300

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production' || env('FORCE_HTTPS', false)) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
         
         HealthTip::observe(HealthTipObserver::class);
